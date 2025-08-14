6201283a31ef28f63f5fbe1c34f42583cbbe60f1
Venkata Subba Reddy Peddireddy <153273816+venkatasubbareddy1@users.noreply.github.com>
2025-08-14 12:23:29 +0530

    Update RAG app.py

--- RAG app.py
+++ RAG app.py
@@ -4,7 +4,7 @@ import os
 from langchain_groq import ChatGroq
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain.chains.combine_documents import create_stuff_documents_chain
-from langchain_core.prompts import ChatPrompTemplate
+from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains import create_retrieval_chain
 from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import PyPDFLoader
